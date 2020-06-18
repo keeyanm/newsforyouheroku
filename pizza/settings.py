ba7a06966179ea7a45b89fd135d397108ff862c9
keeyanm <keeyan2502@gmail.com>
2020-06-18 20:07:20 +0530

    'first-commit'

--- pizza/settings.py
+++ pizza/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = '/Users/Keeyan/OneDrive/Documents/CODING/Harvard Course/final project (news)/orders/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = '/Users/Keeyan/OneDrive/Documents/CODING/Harvard Course/final project (news)/orders/static'
 
 # django_heroku.settings(locals())
