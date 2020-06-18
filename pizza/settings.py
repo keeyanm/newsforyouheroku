0b81eda624451b17806e96b3ca68e762cf537605
keeyanm <keeyan2502@gmail.com>
2020-06-18 19:57:46 +0530

    'first-commit'

--- pizza/settings.py
+++ pizza/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = '/Users/Keeyan/OneDrive/Documents/CODING/Harvard Course/final project (news)/orders/static'
 
 # django_heroku.settings(locals())
