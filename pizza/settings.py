a9b341c9f4faa92a299046d1a1e9ea8e339368b9
keeyanm <keeyan2502@gmail.com>
2020-06-18 13:52:07 +0530

    'first-commit'

--- pizza/settings.py
+++ pizza/settings.py
@@ -120,8 +120,4 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"), # That's it!!
-    '/var/www/static/',
-)
 # django_heroku.settings(locals())
